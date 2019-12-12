3c84cb7d0911516b2c1edb3bdf3d9291dc2a0b44
Luis Nava <levacarrillo@gmail.com>
2019-12-12 11:54:05 -0600

    Before sixth revision

--- Index.cc
+++ Index.cc
@@ -23,14 +23,14 @@ int main()
 	cout << "\033[1;37m	1.4 Objetivos\033[0m\n";
 	cout << "\033[1;37m	1.5 Descripción del documento\033[0m\n";
 	//----------------Antecedentes---------------------
-	cout << "\033[1;37m2. Antecedentes\033[0m\n";
+	cout << "\033[1;33m2. Antecedentes\033[0m\n";
 	cout << "\033[1;33m	2.1 Robots bípedos\033[0m\n";
-	cout << "\033[1;37m	2.2 Aplicaciones en el juego de Futbol Soccer\033[0m\n";
-	cout << "\033[1;33m	2.3 Conceptos básicos de visión computacional\033[0m\n";
+	cout << "\033[1;33m	2.2 Aplicaciones en el juego de Futbol Soccer\033[0m\n";
+	cout << "\033[1;33m	2.3 Usos de la visión computacional\033[0m\n";
 	cout << "\033[1;33m	2.4 El Filtro de Kalman\033[0m\n";
-	cout << "\033[1;37m	2.5 Trabajo relacionado\033[0m\n";
+	cout << "\033[1;33m	2.5 Trabajo relacionado\033[0m\n";
 	//-----------Segmentación de imágenes-------------
-	cout << "\033[1;37m3. Segmentación de imágenes con base en color\033[0m\n";
+	cout << "\033[1;33m3. Segmentación de imágenes con base en color\033[0m\n";
 	cout << "\033[1;33m	3.1 El modelo de cámara Pinhole\033[0m\n";
 	cout << "\033[1;33m	3.2 Corrección de la distorsión\033[0m\n";
 	cout << "\033[1;33m	3.3 Los espacios de color RGB y HSV\033[0m\n";
@@ -42,7 +42,7 @@ int main()
 	cout << "\033[1;37m	4.3 Obtención de los parámetros\033[0m\n";
 	//----------------Implementación------------------
 	cout << "\033[1;37m5. Implementación\033[0m\n";
-	cout << "\033[1;37m	5.1 El robot bípedo Nimbro OP\033[0m\n";
+	cout << "\033[1;33m	5.1 El robot bípedo Nimbro OP\033[0m\n";
 	cout << "\033[1;33m	5.2 Las bibliotecas OpenCV\033[0m\n";
 	cout << "\033[1;33m	5.3 La plataforma ROS\033[0m\n";
 	cout << "\033[1;37m	5.4 Integración de los diferentes programas\033[0m\n";
@@ -55,8 +55,6 @@ int main()
 	cout << "\033[1;37m7. Discusión\033[0m\n";
 	cout << "\033[1;37m	7.1 Conclusiones\033[0m\n";
 	cout << "\033[1;37m	7.2 Trabajo futuro\033[0m\n";
-
-
 
 	return 0;
 }
